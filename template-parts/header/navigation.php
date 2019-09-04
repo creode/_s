d38872a80093aff32baa59217290afff737f6896
Ben Leonard <ben.leonard@creode.co.uk>
2019-09-04 10:06:00 +0100

    added menu location class to menu wrapper so internal elements can be styled contextually

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -5,13 +5,13 @@
     }
 ?>
 
-<div class="header__navigation-wrapper">
+<div class="header__navigation-wrapper header__navigation-wrapper--<?php echo $template_arguments['menu_location']; ?>">
     <?php if(isset($template_arguments['include_toggle']) && $template_arguments['include_toggle'] == true): ?>
         <button class="header__navigation-toggle">
             Open / Close
         </button>
     <?php endif; ?>
-    <nav class="header__navigation header__navigation--<?php echo $template_arguments['menu_location']; ?>">
+    <nav class="header__navigation">
         <?php
             wp_nav_menu(array(
                 'theme_location' => $template_arguments['menu_location']
