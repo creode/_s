1b907e05c84550176c2b02f4d5dbb5d50e334fff
Ben Leonard <ben.leonard@creode.co.uk>
2019-09-10 13:38:27 +0100

    replaced the footer with do_action functionality

--- template-parts/footer/main.php
+++ template-parts/footer/main.php
@@ -1,10 +1,9 @@
+<?php do_action('footer_before'); ?>
 <footer class="footer">
-    <?php
-        add_action('footer_content', function() {
-            creode_wordpress_base_theme_get_template_part('template-parts/footer/navigation', null, array(
-                'menu_location' => 'menu-1'
-            ));
-        }, 100);
-    ?>
-    <?php do_action('footer_content'); ?>
+    <?php do_action('footer_content_before'); ?>
+    <div class="footer__contents">
+        <?php do_action('footer_content'); ?>
+    </div>
+    <?php do_action('footer_content_after'); ?>
 </footer>
+<?php do_action('footer_after'); ?>
